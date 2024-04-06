2d2fb7d32c446d817a3ed688c8a80cd18ce32ae8
Ilya <pankovskiy.im@gmail.com>
2024-04-06 22:53:39 +0300

    add delete node

--- linked_list_simply_connected.py
+++ linked_list_simply_connected.py
@@ -13,9 +13,21 @@ def print_node(vertex):
         vertex = vertex.next
     return None
 
+def del_node(vertex, idx):
+    idx -= 1
+    while idx:
+        print(vertex.value)
+        vertex = vertex.next
+        idx -= 1
+    vertex.next = vertex.next.next
+    return vertex
 
-n3 = Node('value3')
+
+n5 = Node('value5')
+n4 = Node('value4', n5)
+n3 = Node('value3', n4)
 n2 = Node('value2', n3)
 n1 = Node('value1', n2)
 print(print_node(n1))
+del_node(n1, 1)
 print(print_node(n1))
